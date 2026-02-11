889f4378fe8d9c283611df3ab87b00ac626f0590
Desd.py <github.com.passerby609@passfwd.com>
2026-02-11 18:09:19 +0100

    Update startOllama.sh

--- startOllama.sh
+++ startOllama.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-OLLAMA_HOST=0.0.0.0 ollama serve &
+OLLAMA_CONTEXT_LENGTH=32768 OLLAMA_HOST=0.0.0.0 ollama serve &
 sudo nixos-firewall-tool open tcp 11434
-ollama run qwen3-vl:8b-instruct-q4_K_M --keepalive -2m
+ollama run huihui_ai/qwen3-vl-abliterated:30b-a3b-instruct-q4_K_M --keepalive -2m
